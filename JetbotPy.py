61f19422ed9bbbafb3ff785fc3f17720f8484016
ZionDeng <569598401@qq.com>
2021-03-06 11:51:03 +0800

    data computation for movement seems to be working

--- JetbotPy.py
+++ JetbotPy.py
@@ -1,19 +1,100 @@
+from math import tan,sin,cos,atan 
+
 class Jetbot():
     '''
     Jetbot class for jetbot movement 
     '''
+    def __init__(self, position, grabber_p):
+        self.position = list(position)
+        self.heading =  self.checkHeading(grabber_p,position)
+        self.visited = [self.position + [self.heading]]
+        self.Horizon = 15  # A tuning parameter 
+        self.Angle = 3.1415 / 12  # Turning angle at each step 
+        self.StepLen = 5
 
     def right(self):
-        print('right')
-
+        self.heading -= self.Angle
+        self.visited.append(self.position + [self.heading])
 
     def left(self):
-        print('left')
-
+        self.heading += self.Angle
+        self.visited.append(self.position + [self.heading])
 
     def forward(self):
-        print('forward')
+        x, y = self.position 
+        x += int(self.StepLen * cos(self.heading))
+        y += int(self.StepLen * sin(self.heading))
+        self.position = [x,y]
+        self.visited.append([x,y,self.heading])
+        
 
 
     def back(self):
+        """ It might not be used in real-time movement """
+        x, y = self.position 
+        x -= 10 
+        y -= int(10 * tan(self.heading))
+        self.position = [x,y]
         print('backward')
+
+    def jetbot_step(self, cmds, obs_set):
+        """ A step function for jetbot simulation 
+        First turn to the desired direction 
+        Then move forward in that direction 
+        :cmd: the list of path solved by Astar 
+        :obs_ls: the list of obstacles """
+        target_point = cmds[-self.Horizon]
+        target_heading = self.checkHeading(target_point,self.position)
+        while abs(self.heading - target_heading) > 0.27:
+            if target_heading > self.heading:
+                self.left() 
+            else:
+                self.right() 
+        if not self.can_step(obs_set):  # check if one step further will collide to choose the right side
+            if target_heading > self.heading:  
+                self.left() 
+            else: 
+                self.right()
+        self.forward() 
+        print('Position: ',self.position, 
+            'Heading: ', self.heading *360/(2*3.14),
+            'Target Point: ', target_point)
+        
+    def get_trajectory(self):
+        return self.visited
+
+    def get_position(self):
+        return tuple(self.position)
+
+    def checkHeading(self, pHead, pBody):
+        ''' check where the jetbot is heading for
+        :return: the theta of heading angle '''
+        pi = 3.1415926
+        # pHead = objs['Target'][0] 
+        # pBody = objs['Jetbot'][0] 
+        x1, y1 = pHead[0], pHead[1] 
+        x2, y2 = pBody[0], pBody[1] 
+        dx, dy = x1-x2, y1-y2 
+        if dx == 0: 
+            return pi if dy > 0 else -pi 
+        if dx > 0:  # first quadrant and forth quadrant 
+            return atan((y1-y2)/(x1-x2))
+        if dx < 0 and dy > 0:  # second quadrant 
+            return atan((y1-y2)/(x1-x2)) + pi  
+        if dx < 0 and dy < 0:  # third quadrant 
+            return atan((y1-y2)/(x1-x2)) - pi 
+    
+    def can_step(self, obs): 
+        """ check if jetbot can go in that direction
+        Since one step further can collide
+        :obs_set: the Obstacle set (same idea as that in Astar env) """
+        x_test, y_test = self.position
+        x_test += int(self.StepLen * cos(self.heading))
+        y_test += int(self.StepLen * sin(self.heading)) 
+        return False if (x_test,y_test) in obs else True 
+
+if __name__ == '__main__':
+    Pi = 3.1415
+    print(Pi/12  * 180/ Pi)
+    print(-29.418124371804982 * Pi / 180)
+    
--- Testing.py
+++ Testing.py
@@ -1,17 +1,66 @@
 from JetbotPy import Jetbot
-from KeyController import Control
+from Astar import Astar
+from Path_Utils import plotting  
 
-bot = Jetbot() 
-c = Control() 
-c.isCtrling = True  
+def Astar_search(objects, jetbot):
+    """ Searching conducting
+    :return: path points """
+    obstacle_ls = objects['Obstacle']
+    s_start = jetbot.get_position() 
+    s_goal = objects['Target'][0] 
+    if type(obstacle_ls[0]) == type(()):  # if there is only one obstacle:
+        obstacle_ls = [obstacle_ls]
 
-while c.isCtrling:
-    # OR you can directly change the cmd here by 1,2,3,4,,,
-    cmd = c.getdir()
-    if cmd == 1: bot.forward()
-    elif cmd == 2: bot.left()
-    elif cmd == 3: bot.right()
-    elif cmd == 4: bot.back()
-    else:
-        print('Wrong number, Lost Control')
-        c.isCtrling = False
+    astar = Astar(s_start, s_goal, obstacle_ls)
+    path_sol, visited = astar.searching()
+    return path_sol 
+
+def get_obs_set(obstacle_list):
+    if obstacle_list == []:
+        raise ValueError('Obstacle list is empty')
+    obs = set() 
+    for o in obstacle_list:
+        left, right, top, bottom = o[-4:]  # the 4 parameters of the obstacle 'box'
+        for x in range(left, right+1):
+            for y in range(bottom, top+1):
+                obs.add((x,y))
+
+    return obs 
+
+# objects = {'Jetbot': [(95, 46), 83, 107, 63, 28], 
+#     'Obstacle': [(110, 30), 100,120,80,0], 
+#     'Target': [(134, 17), 130, 137, 24, 9], 
+#     'Grabber': [(105, 62), 100, 110, 72, 52]}
+
+objects =  {'Jetbot': [(210, 462), 107, 314, 577, 347], 
+                    'Obstacle': [(758, 292), 693, 823, 388, 197], 
+                    'Target': [(1070, 199), 1036, 1105, 256, 143], 
+                    'Grabber': [(174, 591), 141, 207, 660, 523]}
+
+jetbot_pos = objects['Jetbot'][0]
+grab_pos = objects['Grabber'][0]
+bot = Jetbot(jetbot_pos,grab_pos)
+
+obstacle_ls = objects['Obstacle']
+s_start = objects['Jetbot'][0]
+s_goal = objects['Target'][0] 
+if type(obstacle_ls[0]) == type(()):  # if there is only one obstacle:
+    obstacle_ls = [obstacle_ls]
+astar = Astar(s_start, s_goal, obstacle_ls)
+Original_path, visited = astar.searching()
+
+plot = plotting.Plotting(s_start, s_goal, obstacle_ls)
+plot.animation(Original_path,visited,'AStar')
+
+path = Original_path
+obs_set = get_obs_set(obstacle_ls)
+
+while len(path) > 15:
+    bot.jetbot_step(path,obs_set)
+    path = Astar_search(objects, bot) 
+
+print('Terminate')
+trajectory = bot.get_trajectory()
+# print(trajectory)
+
+
